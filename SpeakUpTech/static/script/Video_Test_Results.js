dc94f665a92e2b437570526d1ed352ad99218633
nageshab <nageshbhavikatti017@gmail.com>
2025-04-05 01:37:25 +0530

    first commit

---
+++ SpeakUpTech/static/script/Video_Test_Results.js
@@ -0,0 +1,103 @@
+fetch('video_results')
+  .then(response => response.json())
+  .then(data => {
+    // Do something with the retrieved data
+    console.log(data);
+    var ctx = document.getElementById("myChart").getContext("2d");
+    // var data2 = [10, 20, 30, 40, 50, 60, 70, 80];
+    var myChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: ['angry', 'dusgust', 'fear', 'happy', 'sad', 'surprise', 'neutral', 'no_face'],
+        datasets: [{
+          data: data[1],
+          backgroundColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)',
+            'rgba(145, 195, 219, 1)',
+            'rgba(180, 221, 220, 1)'
+          ],
+          hoverOffset: 4
+        }]
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'Pie Chart',
+          responsive: true,
+          maintainAspectRatio: false,
+        }
+      }
+    });
+
+    // Get the container div element
+var tableContainer = document.getElementById("myTable");
+
+    // Create a table element
+    var table = document.createElement("table");
+
+    // Create the table headings row
+  var headingsRow = document.createElement("tr");
+  var blankHeading = document.createElement("th");
+  // headingsRow.appendChild(blankHeading); // add blank cell to top left corner
+  for (var i = 0; i < 4; i++) {
+    var heading = document.createElement("th");
+    if(i==0)
+    {
+      var headingText = document.createTextNode("Question");
+      heading.appendChild(headingText);
+      headingsRow.appendChild(heading);
+    }
+    if(i==1)
+    {
+      var headingText = document.createTextNode("Prefered Answer");
+      heading.appendChild(headingText);
+      headingsRow.appendChild(heading);
+    }
+    if(i==2)
+    {
+      var headingText = document.createTextNode("Your Answer");
+      heading.appendChild(headingText);
+      headingsRow.appendChild(heading);
+    }
+    if(i==3)
+    {
+      var headingText = document.createTextNode("Similarity");
+      // headingText = (headingText * 100).toFixed(2) + '%'
+      heading.appendChild(headingText);
+      headingsRow.appendChild(heading);
+    }
+  }
+  table.appendChild(headingsRow);
+
+    // Loop through the arrays and create table rows and cells
+    for (var i = 0; i < 10; i++) {
+      var row = document.createElement("tr");
+      for (var j = 0; j < 4; j++) {
+        var cell = document.createElement("td");
+        var cellText = '';
+        if(j==3){
+          var similarity = parseFloat(data[i][j]);
+          var similarityPercentage = (similarity * 100).toFixed(2) + '%';
+          cellText = document.createTextNode(similarityPercentage);
+        }
+        else
+        {
+          cellText = document.createTextNode(data[i][j]);
+        }
+        // console.log(cellText)
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+      }
+      // console.log("newrow")
+      table.appendChild(row);
+    }
+
+    // Append the table to the container div element
+    tableContainer.appendChild(table);
+  })
+  .catch(error => console.error(error));
